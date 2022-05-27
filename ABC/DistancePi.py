522fcd0585771b9586eb02a92c6b6ef403497ae5
ZEKE <pabo1849@colorado.edu>
2022-05-27 17:32:20 -0600

    logpdf Pi works

--- ABC/DistancePi.py
+++ ABC/DistancePi.py
@@ -15,31 +15,18 @@ dset_001 = f['001']
 Pi = dset_001['Pi']	
 # print(list(Pi.keys())) # -->['edges', 'hist'] # Edges are the x s 
 edges= Pi['edges']
+edges= edges[...]
+x = (edges[1:] + edges[:-1]) / 2
 hist = Pi['hist']
+# Plot pdf (normalized histogram)
+# plt.hist(hist, edges, density=11) 
+# plt.show()
 
-# print (edges)
-# print (hist[...])
-# print (edges[...])
-# print(Pi.shape)
-
-
-
-# 3. Compute the log of the pdf of their summary statistics (S_LES & S_DNS).
-
-def normalize_hist(hist, bins, range):	# range= [-15,5]
-    # the values of the edges with hist? 
-    x = (edges[1:] + edges[:-1]) / 2
-    norm = np.divide(np.sum(hist),bins)
-    # norm = np.sum(pdf)/bins
-    return np.divide (hist,norm)
-    # return pdf/norm
-
-# def pdf_from_array_with_x(array, bins, range):
-#     pdf, edges = np.histogram(array, bins=bins, range=range, density=1)
-#     # How does it asociate the values of the edges with hist? 
-#     x = (edges[1:] + edges[:-1]) / 2
-#     return x, pdf
-
+# Compute log of pdf 
+# Normalize 
+norm = np.divide(np.sum(hist),len(edges))
+hist_pdf= np.divide (hist,norm)
+# Log
 TINY_log = np.log(10e-8)
 
 def take_safe_log(x):
@@ -50,22 +37,61 @@ def take_safe_log(x):
     log = np.log(x, out=log_fill, where=x > TINY)
     return log
 
-# Plot log pdf of production rate
-def LES_logpdf_production():
-    production_pdf= normalize_hist(hist, 100, [-15, 5])
-    production_logpdf = take_safe_log(hist)
-    # print (production_logpdf.shape)
-    plt.style.use('fivethirtyeight')
-    plt.title('log pdf of LES and DNS production rate ')
-    # plt.plot(x_dns[1], y_dns[3], label='DNS', c= 'k', lw= 1.5)
-    # plt.plot( x_dns[1], p_Smag_logpdf, label='Smagorinsky', c= 'g', lw= 1.5)
-    plt.plot(edges, production_logpdf, ls='--', c= 'r', lw= 2) 
-    plt.ylim([-5,5])
-    plt.legend()
-    plt.show()
-    # plt.hist(self.production_data, bins='auto', range=[-1.2, 0.01], density=1)
+hist_log_pdf= take_safe_log(hist_pdf)	# It might need 2 arrays
+plt.plot(x, hist_log_pdf, ls='-', c= 'b', lw= 2)
+plt.ylim([-5,5])
+plt.show()
+# print (edges)
+# print (hist_log_pdf)
 
-LES_logpdf_production()
+# print (hist[...])
+# print (edges[...])
+# print(Pi.shape)
+
+
+
+# 3. Compute the log of the pdf of their summary statistics (S_LES & S_DNS).
+
+# def normalize_hist(hist, bins, range):	# range= [-15,5]
+#     # the values of the edges with hist? 
+#     x = (edges[1:] + edges[:-1]) / 2
+#     norm = np.divide(np.sum(hist),bins)
+#     # norm = np.sum(pdf)/bins
+#     return np.divide (hist,norm)
+#     # return pdf/norm
+
+# # def pdf_from_array_with_x(array, bins, range):
+# #     pdf, edges = np.histogram(array, bins=bins, range=range, density=1)
+# #     # How does it asociate the values of the edges with hist? 
+# #     x = (edges[1:] + edges[:-1]) / 2
+# #     return x, pdf
+
+# TINY_log = np.log(10e-8)
+
+# def take_safe_log(x):
+#     log_fill = np.empty_like(x)         # Why this? 
+#     log_fill.fill(TINY_log)
+#     TINY= np.empty_like(x)
+#     TINY[:] = 10e-8               
+#     log = np.log(x, out=log_fill, where=x > TINY)
+#     return log
+
+# # Plot log pdf of production rate
+# def LES_logpdf_production():
+#     production_pdf= normalize_hist(hist, 100, [-15, 5])
+#     production_logpdf = take_safe_log(hist)
+#     # print (production_logpdf.shape)
+#     plt.style.use('fivethirtyeight')
+#     plt.title('log pdf of LES and DNS production rate ')
+#     # plt.plot(x_dns[1], y_dns[3], label='DNS', c= 'k', lw= 1.5)
+#     # plt.plot( x_dns[1], p_Smag_logpdf, label='Smagorinsky', c= 'g', lw= 1.5)
+#     plt.plot(production_logpdf, ls='--', c= 'r', lw= 2) 
+#     plt.ylim([-5,5])
+#     plt.legend()
+#     plt.show()
+#     # plt.hist(self.production_data, bins='auto', range=[-1.2, 0.01], density=1)
+
+# LES_logpdf_production()
 
 
 
